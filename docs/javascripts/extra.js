b5a695a9fcf110e546ea4767ef07504014dc5c61
Alkis Georgopoulos <alkisg@gmail.com>
2023-08-26 17:52:17 +0300

    Update year

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -1,10 +1,1 @@
-/*
-Not needed anymore
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelectorAll('.right33').forEach(function(pic) {
-    console.log(pic);
-    pic.outerHTML = '<a href="' + pic.src + '">' + pic.outerHTML +
-      '</a>';
-  });
-}, false);
-*/
+/* Nothing here, move on... :) */
